95b464777045541f85f4a9e2c1d7ed47144568ba
mohamed657 <Mohamedhany123@>
2021-11-01 15:38:43 +0200

    finshed design RadioTab
    added radio image
    added all icon

--- lib/radio/radioTab.dart
+++ lib/radio/radioTab.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:untitled/main.dart';
 
 class radioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.orange,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/image/radio_tab.png'),
+          SizedBox(
+            height: 40,
+          ),
+          Text(
+            'اذاعة القرأن الكريم',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ImageIcon(
+                AssetImage('assets/image/Icon metro-next.png'),
+                color: MyThemeData.primaryColor,
+              ),
+              ImageIcon(
+                AssetImage('assets/image/Icon awesome-play.png'),
+                color: MyThemeData.primaryColor,
+              ),
+              ImageIcon(
+                AssetImage('assets/image/Icon metro-next-1.png'),
+                color: MyThemeData.primaryColor,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
+/*
+ ImageIcon(AssetImage(''))
+ */
